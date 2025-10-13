6d1ce47a4f4dc230ff3ebb1692eb841373624b3e
JungHoonKim-KR <100742423+JungHoonKim-KR@users.noreply.github.com>
2025-10-13 23:00:22 +0900

    test

--- src/main/java/com/example/dmfp/OPTApplication.java
+++ src/main/java/com/example/dmfp/OPTApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DmfpApplication {
+public class OPTApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DmfpApplication.class, args);
+        SpringApplication.run(OPTApplication.class, args);
     }
 
 }
